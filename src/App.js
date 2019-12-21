817ec8baa3b46baaccdfb347ba365c08d134f594
David White <dwhite0627@gmail.com>
2019-12-21 18:41:49 -0500

    Separate display panel state from calculation state in App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
-  const [calcState, setCalcState] = useState(0)
+  const [calcState, setCalcState] = useState(0);
+  const [displayState, setDisplayState] = useState(0);
 
   return (
     <div className="container">
