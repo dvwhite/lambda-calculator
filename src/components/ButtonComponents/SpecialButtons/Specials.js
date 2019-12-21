fd868f926915ee80c68e629e244e3526bf1d6bbd
David White <dwhite0627@gmail.com>
2019-12-21 16:56:50 -0500

    Finish styling calcultor

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -6,12 +6,11 @@ import SpecialButton from './SpecialButton';
 
 //Import your array data to from the provided data file
 
-const Specials = () => {
+const Specials = (props) => {
   // STEP 2 - add the imported data to state
   const [specialState, setSpecialState] = useState(specials);
-
   return (
-    <div>
+    <div class={props.className}>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
