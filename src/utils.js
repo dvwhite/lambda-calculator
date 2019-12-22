3743d3514ef9bff99daeb0a0f64dbae8b6fa257e
David White <dwhite0627@gmail.com>
2019-12-22 12:37:24 -0500

    Add support for special characters to the button click handler

--- src/utils.js
+++ src/utils.js
@@ -7,30 +7,37 @@ import { operators } from "./data";
 */
 const handleClick = (event, cb, state) => {
    /* We need to validate number before updating state
-   *  in order to prevent append or eval issues */
+      in order to prevent append or eval issues */
    const value = event.target.textContent;
 
    switch (isNaN(value)) {
    // If number is a special operator, it is NaN
     case true:
     /* If the last char is not Nan, then append the
-    *  period, special or operator.
-    *  _Note_: the numbers read from right to left in
-    *  the display panel of the calculator! */
-      const lastChar = state.split('')[0];
+       period, special or operator.
+       _Note_: the numbers read from right to left in
+       the display panel of the calculator!*/
+      const lastChar = state.split('')[state.length - 1];
       const operatorChars = operators.map(operator => operator.char);
-      if (value === 'C') {
+      const operatorValues = operators.map(operator => operator.value);
+      const operatorObj = {};
+      operatorChars.forEach((key, i) => operatorObj[key] = operatorValues[i]);
+  
+      if (value.toUpperCase() === 'C') {
         cb('0');
+      } else if (value === '=') {
+        // TODO: computation function here
       } else if (value === '.') {
         const decimals = (state.match(/\./g) || []).length;
         if (!isNaN(lastChar) && !decimals) {
             cb(state + value);
         }
       // Prevent consecutive operators
-      } else if (operatorChars.includes(value)) {
-
-        if (!operators.includes(lastChar)) {
-          cb(state + value);
+      } else if (operatorValues.includes(operatorObj[value])) {
+        if (!operatorValues.includes(lastChar)) {
+          cb(state + operatorObj[value]);
+        } else {
+          cb(state.substring(0, state.length - 1) + operatorObj[value]);
         }
       }
       break;
