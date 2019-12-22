1e26240ca0a18735cc24f3bf203295f5c9593384
David White <dwhite0627@gmail.com>
2019-12-22 09:48:59 -0500

    Use spread operator with props in all button components

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 const OperatorButton = (props) => {
-  const {value, onClick, className} = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className={className} onClick={onClick}>{value}</button>
+      <button {...props}>{props.value}</button>
     </>
   );
 };
