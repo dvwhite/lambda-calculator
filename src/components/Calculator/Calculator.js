599e125b042d6d7d731e2fdfc3fff07be099e993
David White <dwhite0627@gmail.com>
2019-12-24 06:56:41 -0500

    Begin adding in support for two displays

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -3,14 +3,16 @@ import Display from '../../components/DisplayComponents/Display';
 import Numbers from '../../components/ButtonComponents/NumberButtons/Numbers'
 import Operators from '../../components/ButtonComponents/OperatorButtons/Operators';
 import Specials from '../../components/ButtonComponents/SpecialButtons/Specials';
-import handleClick from '../../utils';
+import handleClick, {updateDisplay} from '../../utils';
 
 const Calculator = (props) => {
   const [calcState, setCalcState] = useState('0');
   const [displayState, setDisplayState] = useState('0');
+  const [usedDecimal, setUsedDecimal] = useState(false);
 
   const onButtonClick = (event) => {
-    return handleClick(event, setCalcState, calcState);
+    handleClick(event, setCalcState, calcState);
+    updateDisplay(event, setDisplayState, displayState, setUsedDecimal, usedDecimal);
   }
 
   return (
