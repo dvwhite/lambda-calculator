7e71016c8b647605b8e866dce23053760758ebb9
David White <dwhite0627@gmail.com>
2019-12-24 06:56:22 -0500

    Move const to top of handleClick function

--- src/utils.js
+++ src/utils.js
@@ -10,28 +10,33 @@ const handleClick = (event, cb, state) => {
    /* We need to validate number before updating state
       in order to prevent append or eval issues */
    const value = event.target.textContent;
+   const lastChar = state.split('')[state.length - 1];
+   const operatorChars = operators.map(operator => operator.char);
+   const operatorValues = operators.map(operator => operator.value);
+   const operatorObj = {};
+   operatorChars.forEach((key, i) => operatorObj[key] = operatorValues[i]);
+   // const numbersInState = state.split('').filter(char => numbers.includes(char))
+   // const operatorsInState = state.split('').filter(char => operators.includes(char))
 
    switch (isNaN(value)) {
    // If number is a special operator, it is NaN
     case true:
-    /* If the last char is not Nan, then append the
-       period, special or operator.
-       _Note_: the numbers read from right to left in
-       the display panel of the calculator!*/
-      const lastChar = state.split('')[state.length - 1];
-      const operatorChars = operators.map(operator => operator.char);
-      const operatorValues = operators.map(operator => operator.value);
-      const operatorObj = {};
-      operatorChars.forEach((key, i) => operatorObj[key] = operatorValues[i]);
+    /* If the last char is not NaN, then append the
+       period, special or operator.*/
   
+      // The clear function
       if (value.toUpperCase() === 'C') {
         cb('0');
+        // The change sign function
+      } else if (value === '+/-') {;
+        cb((-state).toString());
+        // The equals function
       } else if (value === '=') {
-        // TODO: computation function here
         if (!isNaN(lastChar)) {
           const result = math.evaluate(state);
           cb(result.toString());
         }
+        // Prevent multiple decimal points
       } else if (value === '.') {
         const decimals = (state.match(/\./g) || []).length;
         if (!isNaN(lastChar) && !decimals) {
@@ -60,4 +65,15 @@ const handleClick = (event, cb, state) => {
     }
 }
 
+const updateDisplay = (event, cb, state) => {
+  const value = event.target.textContent;
+  const lastChar = state.split('')[state.length - 1];
+  const operatorChars = operators.map(operator => operator.char);
+  const operatorValues = operators.map(operator => operator.value);
+  const operatorObj = {};
+  operatorChars.forEach((key, i) => operatorObj[key] = operatorValues[i]);
+  
+}
+
 export default handleClick;
+export {updateDisplay};
