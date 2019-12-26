2fa580e5437148b87675ecc44eb0c7b9596940ad
David White <dwhite0627@gmail.com>
2019-12-25 22:11:47 -0500

    Add incremental computation and display controlled by clicking operators

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,17 +1,25 @@
 import React, {useState} from 'react';
 import Display from '../../components/DisplayComponents/Display';
-import Computation from '../../components/DisplayComponents/Computation';
 import Numbers from '../../components/ButtonComponents/NumberButtons/Numbers'
 import Operators from '../../components/ButtonComponents/OperatorButtons/Operators';
 import Specials from '../../components/ButtonComponents/SpecialButtons/Specials';
-import handleClick, {updateDisplay} from '../../utils';
+import handleClick from '../../utils';
 
 const Calculator = (props) => {
-  const [calcState, setCalcState] = useState('0');
+  const [calcState, setCalcState] = useState('');
   const [displayState, setDisplayState] = useState('0');
+  const [operatorClicked, setOperatorClicked] = useState(false);
 
   const onButtonClick = (event) => {
-    handleClick(event, setCalcState, calcState, setDisplayState, displayState);
+    handleClick(
+      event, 
+      setCalcState, 
+      calcState, 
+      setDisplayState, 
+      displayState, 
+      operatorClicked, 
+      setOperatorClicked
+      );
   }
 
   return (
--- src/utils.js
+++ src/utils.js
@@ -6,80 +6,89 @@ import * as math from 'mathjs';
 * @param {number} number: The number to insert
 * returns: none
 */
-const handleClick = (event, setCalcState, calcState, setDisplayState, displayState) => {
+const handleClick = (event, setCalcState, calcState, setDisplayState, 
+                     displayState, operatorClicked, setOperatorClicked) => {
    /* We need to validate number before updating calcState
       in order to prevent append or eval issues */
-   const value = event.target.textContent;
-   const lastChar = calcState.split('')[calcState.length - 1];
-   const operatorChars = operators.map(operator => operator.char);
-   const operatorValues = operators.map(operator => operator.value);
-   const operatorObj = {};
-   operatorChars.forEach((key, i) => operatorObj[key] = operatorValues[i]);
-   // const numbersIncalcState = calcState.split('').filter(char => numbers.includes(char))
-   // const operatorsIncalcState = calcState.split('').filter(char => operators.includes(char))
+  const value = event.target.textContent;
+  const lastCalcChar = calcState.split('')[calcState.length - 1];
+  const lastDisplayChar = displayState.split('')[displayState.length - 1];
+  const operatorChars = operators.map(operator => operator.char);
+  const operatorValues = operators.map(operator => operator.value);
+  const operatorObj = {};
+  const isOperator = operatorChars.includes(value);
+  operatorChars.forEach((key, i) => operatorObj[key] = operatorValues[i]);
+  console.log(isOperator);
 
-   switch (isNaN(value)) {
-   // If number is a special operator, it is NaN
-    case true:
-    /* If the last char is not NaN, then append the
-       period, special or operator.*/
-  
-      // The clear function
-      if (value.toUpperCase() === 'C') {
-        setCalcState('');
-        setDisplayState('0');
-        // The change sign function
-      } else if (value === '+/-') {
-        const match = (/(\d+)(?!.*\d)/.exec(calcState));
-        const lastNum = match[0];
-        const lastNumPos = match.index;
-        const negResult = (-lastNum).toString();
-        if (!isNaN(negResult)) {
-          const newString = calcState.substring(0, lastNumPos) + negResult;
-          setCalcState(newString);
-          if (displayState !== '0') {
-            const result = math.evaluate(newString);
-            setDisplayState(result.toString());
-          }
+  switch (isNaN(value)) {
+  // If number is a period, a special character, or an operator, it is NaN
+  case true:
+  /* If the last char is not NaN, then append the
+      period, special or operator.*/
+
+    // The clear function
+    if (value.toUpperCase() === 'C') {
+      setCalcState('');
+      setDisplayState('0');
+      // The change sign function
+    } else if (value === '+/-') {
+        if (displayState !== '0') {
+          const negValue = (-displayState).toString();
+          setDisplayState(negValue);
         }
-        // The equals function
-      } else if (value === '=') {
-        if (!isNaN(lastChar)) {
-          const result = math.evaluate(calcState);
-          setDisplayState(result.toString());
-          setCalcState(result.toString());
-        }
-        // Prevent multiple decimal points
-      } else if (value === '.') {
-        const decimals = (calcState.match(/\./g) || []).length;
-        if (!isNaN(lastChar) && !decimals) {
-            setCalcState(calcState + value);
-        }
-      // Prevent consecutive operators
-      } else if (operatorValues.includes(operatorObj[value])) {
-        if (!operatorValues.includes(lastChar)) {
-          setCalcState(calcState + operatorObj[value]);
-        } else {
-          setCalcState(calcState.substring(0, calcState.length - 1) + operatorObj[value]);
-        }
+      // The equals function
+    } else if (value === '=') {
+      if (!isNaN(lastCalcChar)) {
+        const result = math.evaluate(calcState);
+        setDisplayState(result.toString());
+        setCalcState(result.toString());
       }
-      break;
-    // Otherwise, it is a number
-    case false:
-    // If calcState is 0, return number, else append number
-      if (calcState === '0') {
-          setCalcState(value);
-          const result = math.evaluate(value);
-          setDisplayState(result.toString());
-      } else {
+      // Prevent multiple decimal points
+    } else if (value === '.') {
+      const decimals = (displayState.match(/\./g) || []).length;
+      if (!isNaN(lastDisplayChar) && !decimals) {
           setCalcState(calcState + value);
-          const result = math.evaluate(calcState + value);
-          setDisplayState(result.toString());
+          setDisplayState(displayState + value);
       }
-      break;
-    default:
-        // Do nothing
+    // Prevent consecutive operators
+    } else if (isOperator) {
+      if (!operatorValues.includes(lastCalcChar)) {
+        setCalcState(calcState + operatorObj[value]);
+      } else {
+        setCalcState(calcState.substring(0, calcState.length - 1) + operatorObj[value]);
+      }
     }
+    break;
+  // Otherwise, it is a number
+  case false:
+    // If calcState is 0, return number, else append number
+    if (calcState === '0' || calcState === '') {
+        setCalcState(value);
+    } else {
+      if (operatorClicked) {
+        setCalcState(calcState + value);
+      }
+    }
+    // If displayState is 0, return number, else append number
+    if (displayState === '0') {
+      setDisplayState(value);
+    } else {
+      if (operatorClicked) {
+        setDisplayState(value);
+      } else {
+        setDisplayState(displayState + value)
+      }
+    }
+    break;
+  default:
+      // Do nothing
+  }
+
+  if (isOperator) {
+    setOperatorClicked(true); // set this to true every time an operator is clicked
+  } else {
+    setOperatorClicked(false); // otherwise resets to false
+  }
 }
 
 export default handleClick;
