ec6d5e2ebca2ea5ea703136532a866ba7799452f
David White <dwhite0627@gmail.com>
2020-01-02 22:37:26 -0500

    Refactor utils.js to improve readability

--- src/utils.js
+++ src/utils.js
@@ -23,90 +23,95 @@ const handleClick = (event, setCalcState, calcState, setDisplayState,
 
   switch (isNaN(value)) {
   // If number is a period, a special character, or an operator, it is NaN
-  case true:
-  /* If the last char is not NaN, then append the
-      period, special or operator.*/
-
-    // The clear function
-    if (value.toUpperCase() === 'C') {
-      setCalcState('');
-      setDisplayState('0');
-      setNewNumberInputReady(false)
-      // The change sign function
-    } else if (value === '+/-') {
-        if (displayState !== '0') {
+    case true:
+    /* If the last char is not NaN, then append the
+        period, special or operator.*/
+      switch(value) {
+        // The clear function
+        case 'C': 
+          setCalcState('');
+          setDisplayState('0');
+          setNewNumberInputReady(false)
+          break;
+          // The change sign function
+        case '+/-':
+          if (displayState !== '0') {
+            if (!isNaN(lastCalcChar)) {
+              const negValue = (-lastNumAdded).toString();
+              setDisplayState(negValue);
+              setCalcState(calcState.substring(0, lastOpIndex + 1) + negValue);
+            } else {
+              setNewNumberInputReady(true)
+            }
+          }
+          break;
+          // The percentage function
+        case '%':
+          if (displayState !== '0') {
+            if (!isNaN(lastCalcChar)) {
+              const percentValue = (lastNumAdded * 0.01).toString();
+              setDisplayState(percentValue);
+              setCalcState(calcState.substring(0, lastOpIndex + 1) + percentValue);
+            } else {
+              setNewNumberInputReady(true)
+            }
+          }
+          break;
+          // The equals function
+        case '=':
           if (!isNaN(lastCalcChar)) {
-            const negValue = (-lastNumAdded).toString();
-            setDisplayState(negValue);
-            setCalcState(calcState.substring(0, lastOpIndex + 1) + negValue);
-          } else {
-            setNewNumberInputReady(true)
+            const result = math.evaluate(calcState);
+            setDisplayState(result.toString());
+            setCalcState(result.toString());
+          }
+          break;
+          // Prevent multiple decimal points
+        case '.':
+          const decimals = (displayState.match(/\./g) || []).length;
+          if (!isNaN(lastDisplayChar) && !(operatorValues.includes(lastCalcChar)) && !decimals) {
+            if (calcState !== '') {
+              setCalcState(calcState + value);
+              setDisplayState(displayState + value);
+            } else {
+              setCalcState(displayState + value);
+              setDisplayState(displayState + value);
+            }
+          }
+          break;
+        // Prevent consecutive operators
+        default:
+          if (calcState !== '' && lastCalcChar !== '.') {
+            if (!operatorValues.includes(lastCalcChar)) {
+              setCalcState(calcState + operatorObj[value]);
+            } else {
+              setCalcState(calcState.substring(0, calcState.length - 1) + operatorObj[value]);
+            }
           }
         }
-      // The percentage function
-    } else if (value === '%') {
-      if (displayState !== '0') {
-        if (!isNaN(lastCalcChar)) {
-          const percentValue = (lastNumAdded * 0.01).toString();
-          setDisplayState(percentValue);
-          setCalcState(calcState.substring(0, lastOpIndex + 1) + percentValue);
-        } else {
-          setNewNumberInputReady(true)
-        }
+        break;
+    // Otherwise, it is a number
+    case false:
+      // If calcState is 0, return number, else append number
+      if (calcState === '0' || calcState === '') {
+          setCalcState(value);
+      } else {
+        setCalcState(calcState + value);
       }
-      // The equals function
-    } else if (value === '=') {
-      if (!isNaN(lastCalcChar)) {
-        const result = math.evaluate(calcState);
-        setDisplayState(result.toString());
-        setCalcState(result.toString());
-      }
-      // Prevent multiple decimal points
-    } else if (value === '.') {
-      const decimals = (displayState.match(/\./g) || []).length;
-      if (!isNaN(lastDisplayChar) && !(operatorValues.includes(lastCalcChar)) && !decimals) {
-        if (calcState !== '') {
-          setCalcState(calcState + value);
-          setDisplayState(displayState + value);
-        } else {
-          setCalcState(displayState + value);
-          setDisplayState(displayState + value);
-        }
-      }
-    // Prevent consecutive operators
-    } else if (isOperator) {
-      if (calcState !== '' && lastCalcChar !== '.') {
-        if (!operatorValues.includes(lastCalcChar)) {
-          setCalcState(calcState + operatorObj[value]);
-        } else {
-          setCalcState(calcState.substring(0, calcState.length - 1) + operatorObj[value]);
-        }
-      }
-    }
-    break;
-  // Otherwise, it is a number
-  case false:
-    // If calcState is 0, return number, else append number
-    if (calcState === '0' || calcState === '') {
-        setCalcState(value);
-    } else {
-      setCalcState(calcState + value);
-    }
-    // If displayState is 0, return number, else append number
-    if (displayState === '0') {
-      setDisplayState(value);
-    } else {
-      if (newNumberInputReady) {
+      // If displayState is 0, return number, else append number
+      if (displayState === '0') {
         setDisplayState(value);
       } else {
-        setDisplayState(displayState + value)
+        if (newNumberInputReady) {
+          setDisplayState(value);
+        } else {
+          setDisplayState(displayState + value)
+        }
       }
+      if (value !== '.') setNewNumberInputReady(false); // otherwise resets to false
+      break;
+    default:
+        // Do nothing
     }
-    if (value !== '.') setNewNumberInputReady(false); // otherwise resets to false
-    break;
-  default:
-      // Do nothing
-  }
 
   if (isOperator) {
     setNewNumberInputReady(true); // set this to true every time an operator is clicked
